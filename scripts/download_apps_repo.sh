c5c5ae197fb7f825f58c6e368024882afd8a0b28
DjP-iX <133042991+DjP-iX@users.noreply.github.com>
2025-08-06 13:42:31 -0400

    import scripts and static

---
+++ scripts/download_apps_repo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define the base directory relative to the current script location
+BASE_DIR=$(dirname "$(realpath "$0")")/../Apps_Temp
+
+# Create the temporary directory with a specific name
+TEMP_DIR="$BASE_DIR"
+
+# Check if the directory already exists, and if so, clean it up
+if [ -d "$TEMP_DIR" ]; then
+  echo "Cleaning up existing temporary directory: $TEMP_DIR"
+  rm -rf "$TEMP_DIR"
+fi
+
+# Create the directory
+mkdir -p "$TEMP_DIR"
+
+# Print a status message indicating the download is starting
+echo "Starting to download the 'trains' directory from the GitHub repository into: $TEMP_DIR"
+
+# Define the GitHub repository URL
+REPO_URL="https://github.com/truenas/apps"
+
+git clone -b master --depth=1 "$REPO_URL" "$BASE_DIR"
+if [ $? -eq 0 ]; then
+  echo "'trains' directory successfully downloaded to: $TEMP_DIR"
+  exit 0
+else
+  echo "Failed to download the 'trains' directory. Please check the URL or your network connection."
+  exit 1
+fi
